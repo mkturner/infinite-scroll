dab50789ec17b5458662fc24f40746528640bdf6
Marvin T <contact@marvinkturner.com>
2020-08-31 21:45:42 -0400

    refactor: add helper function to set attributes, code reuse

--- script.js
+++ script.js
@@ -13,20 +13,31 @@ const orientation = 'landscape';
 const query = 'africa'
 const unsplashApiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&orientation=${orientation}&query=&{query}`;
 
+// Helper function that sets attributes on DOM Elements
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
 // Programmatically create elements for links/photos; manipulate into DOM
 function displayPhotos() {
     // 'forEach': run function for each object in 'photosArray'
     photosArray.forEach((photo) => {
         // Create an anchor to link to original photo
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
         
         // Create img for photo
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        })
         
         // Put img inside a, put a inside image-container
         item.appendChild(img);
